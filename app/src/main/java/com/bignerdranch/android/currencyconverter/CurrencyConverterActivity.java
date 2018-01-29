ade115f1cdcc280174f87a40e801477331689992
Mittal Jethwa <mittaljethwa9383@gmail.com>
2018-01-28 17:47:12 -0800

    "added save instance and restore instance functionality"

--- app/src/main/java/com/bignerdranch/android/currencyconverter/CurrencyConverterActivity.java
+++ app/src/main/java/com/bignerdranch/android/currencyconverter/CurrencyConverterActivity.java
@@ -1,58 +1,68 @@
 package com.bignerdranch.android.currencyconverter;
 
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.text.DecimalFormat;
 
 
 public class CurrencyConverterActivity extends AppCompatActivity {
 
-    EditText usdAmountObject = null;
-    EditText inrAmountObject = null;
-    TextWatcher inrInputListener = null;
-    TextWatcher usdInputListener = null;
-    double usdAmount;
-    double inrAmount;
-    String usdSavedValue;
-    String inrSavedValue;
+    private EditText usdAmountObject = null;
+    private EditText inrAmountObject = null;
+    private String lastEdited = "None";
+    private double usdAmount = -1;
+    private double inrAmount = -1;
+    private final String amountsKey = "amountsKey";
+    private double[] amountsToSave = new double[2];
+    private double[] amountsToRestore = new double[2];
+    private String usdAmountString;
+    private String inrAmountString;
+    private final DecimalFormat roundUptoTwoDecimals = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
 
-        Button convert_button = (Button) findViewById(R.id.convertButtonId);
-        usdAmountObject = (EditText) findViewById(R.id.usdInputId);
-        inrAmountObject = (EditText) findViewById(R.id.inrInputId);
+        Button convertButton = this.findViewById(R.id.convertButtonId);
+        usdAmountObject = this.findViewById(R.id.usdInputId);
+        inrAmountObject = this.findViewById(R.id.inrInputId);
 
-
+        //To ensure usdAmount and inrAmount are changed from default values to last converted values
         if(savedInstanceState != null) {
-            double amounts[];
-            amounts = savedInstanceState.getDoubleArray(usdSavedValue);
-            Log.d("MJ","USD Value on create(OLD):" + amounts[0]);
-            Log.d("MJ","INR Value on create(OLD):" + amounts[1]);
-            usdAmount = amounts[0];
-            inrAmount = amounts[1];
-
+            amountsToRestore = savedInstanceState.getDoubleArray(amountsKey);
+            usdAmount = amountsToRestore[0];
+            inrAmount = amountsToRestore[1];
         }
 
-        convert_button.setOnClickListener(new View.OnClickListener() {
+        convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String usdAmountString = usdAmountObject.getText().toString();
-                String inrAmountString = inrAmountObject.getText().toString();
+                usdAmountString = usdAmountObject.getText().toString();
+                inrAmountString = inrAmountObject.getText().toString();
 
-                if(!usdAmountString.isEmpty() && !inrAmountString.isEmpty()) {
-                    Toast.makeText(CurrencyConverterActivity.this,"Please enter amount in only one of the fields", Toast.LENGTH_LONG).show();
-                    return;
+                //Check if both the text fields are empty
+                if(usdAmountString.isEmpty() && inrAmountString.isEmpty()) {
+                    Toast.makeText(CurrencyConverterActivity.this,R.string.alertEitherFields, Toast.LENGTH_SHORT).show();
+                }
+                else if(usdAmountString.equals(".") || inrAmountString.equals(".")) {
+                    Toast.makeText(CurrencyConverterActivity.this,R.string.alertInvalidInput, Toast.LENGTH_SHORT).show();
+                }
+                //If both the text fields are non-empty, check which field was edited last
+                else if(!usdAmountString.isEmpty() && !inrAmountString.isEmpty()) {
+                    if(lastEdited.equals("USDTextEdited")) {
+                        usdAmount = Double.parseDouble(usdAmountObject.getText().toString());
+                        convertUsdToInr(usdAmount);
+                    }
+                    else if(lastEdited.equals("INRTextEdited")) {
+                        inrAmount = Double.parseDouble(inrAmountObject.getText().toString());
+                        convertInrToUsd(inrAmount);
+                    }
                 }
                 else if(!usdAmountString.isEmpty()) {
                     usdAmount = Double.parseDouble(usdAmountObject.getText().toString());
@@ -62,116 +72,61 @@ public class CurrencyConverterActivity extends AppCompatActivity {
                     inrAmount = Double.parseDouble(inrAmountObject.getText().toString());
                     convertInrToUsd(inrAmount);
                 }
-                else {
-                    Toast.makeText(CurrencyConverterActivity.this,"Please enter amount in either of the two fields", Toast.LENGTH_SHORT).show();
-                    return;
-                }
             }
         });
 
-        usdInputListener = new TextWatcher() {
+        View.OnFocusChangeListener usdEditTextFocusListener = new View.OnFocusChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence text, int start, int count, int after) {
-                Log.d("USD Text Changed",text.toString()+" Length"+text.length());
-                if(text.length() > 0) {
-                    disableEdit(inrAmountObject,inrInputListener);
-                }
-                else {
-                    Log.d("In Else",text.toString()+" Length"+text.length());
-                    enableEdit(inrAmountObject);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus)
+                    lastEdited = "USDTextEdited";
             }
         };
 
-        inrInputListener = new TextWatcher() {
+        View.OnFocusChangeListener inrEditTextFocusListener = new View.OnFocusChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence text, int start, int count, int after) {
-                Log.d("INR Text Changed",text.toString()+" Length"+text.length());
-                if(text.length() > 0) {
-                    disableEdit(usdAmountObject,usdInputListener);
-                }
-                else {
-                    Log.d("In Else",text.toString()+" Length"+text.length());
-                    enableEdit(usdAmountObject);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus)
+                    lastEdited = "INRTextEdited";
             }
         };
 
-//        usdAmountObject.addTextChangedListener(usdInputListener);
-//        inrAmountObject.addTextChangedListener(inrInputListener);
-
+        usdAmountObject.setOnFocusChangeListener(usdEditTextFocusListener);
+        inrAmountObject.setOnFocusChangeListener(inrEditTextFocusListener);
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        Log.i("MJ", "onSaveInstanceState");
-        double amounts[]=new double[2];
-        Log.i("MJ: savedInstance Value",savedInstanceState.toString());
-
-            amounts[0] = usdAmount;
-            amounts[1] = inrAmount;
-            Log.d("MJ","USD Value on save:" + usdAmount);
-            Log.d("MJ","INR Value on save:" + inrAmount);
-            savedInstanceState.putDoubleArray(usdSavedValue,amounts);
-
-
+        amountsToSave[0] = usdAmount;
+        amountsToSave[1] = inrAmount;
+        savedInstanceState.putDoubleArray(amountsKey,amountsToSave);
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.i("MJ", "onRestoreInstanceState");
-        double amounts[] = savedInstanceState.getDoubleArray(usdSavedValue);
-        Log.d("MJ","Double array:" + savedInstanceState.getDoubleArray(usdSavedValue).toString());
-        Log.d("MJ","USD Value on restore:" + amounts[0]);
-        Log.d("MJ","INR Value on restore:" + amounts[1]);
-        usdAmountObject.setText(String.format("%.2f",amounts[0]));
-        inrAmountObject.setText(String.format("%.2f",amounts[1]));
+        amountsToRestore = savedInstanceState.getDoubleArray(amountsKey);
+        //if text fields were blank before rotation then set text fields to blank instead of default values
+        if (amountsToRestore[0] == -1 && amountsToRestore[1] == -1) {
+            usdAmountObject.setText(null);
+            inrAmountObject.setText(null);
+        }
+        else {
+            usdAmountObject.setText(roundUptoTwoDecimals.format(amountsToRestore[0]));
+            inrAmountObject.setText(roundUptoTwoDecimals.format(amountsToRestore[1]));
+        }
     }
 
-    private void disableEdit(EditText objectToDisable, TextWatcher listenerName) {
-        objectToDisable.setFocusable(false);
-        objectToDisable.setEnabled(false);
-        objectToDisable.setCursorVisible(false);
-        objectToDisable.removeTextChangedListener(listenerName);
-    }
-
-    private void enableEdit(EditText objectToEnable) {
-        objectToEnable.setEnabled(true);
-        objectToEnable.setFocusableInTouchMode(true);
-        objectToEnable.setFocusable(true);
-        objectToEnable.setCursorVisible(true);
-    }
+    //Convert values from USD to INR
     private void convertUsdToInr(double usdValue) {
         inrAmount = usdValue * 63.89;
-//        inrAmountObject.setText(Double.toString(inrAmount));
-        inrAmountObject.setText(String.format("%.2f",inrAmount));
+        inrAmountObject.setText(roundUptoTwoDecimals.format(inrAmount));
     }
 
+    //Convert values from INR to USD
     private void convertInrToUsd(double inrValue) {
         usdAmount = inrValue / 63.89;
-//        usdAmountObject.setText(Double.toString(usdAmount));
-        usdAmountObject.setText(String.format("%.2f",usdAmount));
+        usdAmountObject.setText(roundUptoTwoDecimals.format(usdAmount));
     }
 }
